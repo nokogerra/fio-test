546800d59511621143997ace6a286d34dee9e0fe
akopylov <akopylov@nokogerra.lab>
2022-09-16 14:37:57 +0300

    fix

--- 0.controller_packages.sh
+++ 0.controller_packages.sh
@@ -1,8 +1,14 @@
-#!/bing/bash
+#!/bin/bash
+#if there is no proxy in your env, comment the next 2 lines
+sudo bash -c 'echo HTTP_PROXY=http://ip_addr:port >> /etc/environment
+source /etc/environment
 sudo apt update
 sudo apt install -y git
+#if there is no proxy in your env, comment the next line
+git config --global http.proxy http://ip_addr:port
 sudo apt install -y python3-pip
 sudo apt install tmux
-sudo pip3 install ansible jmespath matplotlib iostat-tool
+#if there is no proxy in your env, remove the proxy switch
+sudo pip3 --proxy http://ip_addr:port install ansible jmespath matplotlib iostat-tool
 git init
 git clone https://github.com/nokogerra/fio-test
